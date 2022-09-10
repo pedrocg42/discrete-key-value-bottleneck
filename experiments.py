50312ba175b6666c346b3a4c880f5478ccccc089
pedrocg42 <pedrocorglez@gmail.com>
2022-09-11 01:46:04 +0200

    feat: update experiments

--- experiments.py
+++ experiments.py
@@ -13,39 +13,8 @@ from torch.nn import CrossEntropyLoss
 # Datasets
 from data import CIFAR100
 
-baseline_resnet18_pretrain = {
-    "name": "baseline_resnet18_pretrain",
-    # Model
-    "architecture": DKVB,
-    "architecture_type": "baseline",
-    "encoder_name": "dino_resnet_50",
-    "num_codebooks": 8096,
-    # Data
-    "dataset": CIFAR100,
-    # Train
-    "optimizer": Adam,
-    "learning_rate": 3e-4,
-    "num_epochs_initialization_keys": 10,
-    "num_epochs_initialization": 100,
-}
-
-vq_resnet18_pretrain = {
-    # Model
-    "architecture": DKVB,
-    "architecture_type": "discrete_key_value_bottleneck",
-    "encoder_name": "dino_resnet_50",
-    "num_codebooks": 8096,
-    # Data
-    "dataset": CIFAR100,
-    # Train
-    "optimizer": Adam,
-    "learning_rate": 3e-4,
-    "num_epochs_initialization_keys": 10,
-    "num_epochs_initialization": 100,
-}
-
-dkvb_resnet18_pretrain = {
-    "name": "dkvb_resnet18_pretrain",
+dkvb_resnet18_1_pretrain = {
+    "name": "dkvb_resnet18_1_pretrain",
     # Model
     "architecture": DKVB,
     "architecture_type": "discrete_key_value_bottleneck",
@@ -68,5 +37,57 @@ dkvb_resnet18_pretrain = {
     "learning_rate": 3e-4,
     "batch_size": 100,
     "num_epochs_initialization_keys": 10,
+    "num_epochs": 100,
+}
+
+vq_resnet18_1_pretrain = {
+    "name": "vq_resnet18_1_pretrain",
+    # Model
+    "architecture": DKVB,
+    "architecture_type": "vector_quantized",
+    "encoder_name": "dino_resnet_50",
+    "embedding_dim": 1024,
+    "key_value_pairs": 8192,
+    "num_codebooks": 1,
+    "value_dimension": "same",
+    "vq_decay": 0.9,
+    "threshold_ema_dead_code": 10,  # 0.8 * 100 * 1 * 1 * 1024 / 8192 (0.8·batch-size·h·w·mz/num-pairs)
+    "p_dropout": 0.2,
+    "hidden_dense_layers": [512, 256],
+    "num_classes": 100,
+    # Data
+    "dataset": CIFAR100,
+    "train_val_split": 0.8,
+    # Train
+    "optimizer": Adam,
+    "criteria": CrossEntropyLoss,
+    "learning_rate": 3e-4,
+    "batch_size": 100,
+    "num_epochs": 100,
+}
+
+baseline_resnet18_pretrain = {
+    "name": "baseline_resnet18_pretrain",
+    # Model
+    "architecture": DKVB,
+    "architecture_type": "baseline",
+    "encoder_name": "dino_resnet_50",
+    "embedding_dim": 1024,
+    "key_value_pairs": 8192,
+    "num_codebooks": 1,
+    "value_dimension": "same",
+    "vq_decay": 0.9,
+    "threshold_ema_dead_code": 10,  # 0.8 * 100 * 1 * 1 * 1024 / 8192 (0.8·batch-size·h·w·mz/num-pairs)
+    "p_dropout": 0.2,
+    "hidden_dense_layers": [512, 256],
+    "num_classes": 100,
+    # Data
+    "dataset": CIFAR100,
+    "train_val_split": 0.8,
+    # Train
+    "optimizer": Adam,
+    "criteria": CrossEntropyLoss,
+    "learning_rate": 3e-4,
+    "batch_size": 100,
     "num_epochs": 100,
 }
