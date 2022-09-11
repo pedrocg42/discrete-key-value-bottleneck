2c9e4e2a9e5535b9f8a6b139ab9a6538b233e36c
pedrocg42 <pedrocorglez@gmail.com>
2022-09-11 19:31:55 +0200

    feat: add balanced data and test split feature

--- data.py
+++ data.py
@@ -1,9 +1,12 @@
 import os
 import pickle
+
+import numpy as np
 import torch
+import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torchvision import transforms
-import torch.nn.functional as F
+
 import config
 
 
@@ -18,12 +21,34 @@ class CIFAR100(Dataset):
     def __init__(self, train_val_split: float = 0.8, split: str = "train"):
 
         # Loading raw file
-        file_path = os.path.join(config.dataset_path, "train")
+        if split == "train" or split == "val":
+            file_path = os.path.join(config.dataset_path, "train")
+        elif split == "test":
+            file_path = os.path.join(config.dataset_path, "test")
         with open(file_path, "rb") as fo:
             data_dict = pickle.load(fo, encoding="bytes")
 
-        # Loading and preprocessing images
+        # Loading images and labels
         self.images = data_dict[b"data"]
+        self.labels = np.array(data_dict[b"fine_labels"])
+
+        # Splitting data
+        if split == "train" or split == "val":
+            # Balancing and classes per split
+            self.indexes = np.empty((100, 500), dtype=int)
+            for i in range(100):
+                self.indexes[i] = np.where(self.labels == i)[0]
+
+            index_split = int(train_val_split * self.indexes.shape[1])
+            if split == "train":
+                self.indexes = self.indexes[:, :index_split].flatten()
+            elif split == "val":
+                self.indexes = self.indexes[:, index_split:].flatten()
+
+            self.images = self.images[self.indexes]
+            self.labels = self.labels[self.indexes]
+
+        # Preprocessing images
         self.images = self.images.reshape(self.images.shape[0], 3, 32, 32)
         self.images = self.images / 255.0
         self.images = self.images.astype("float32")
@@ -31,17 +56,8 @@ class CIFAR100(Dataset):
         self.images = self.transform(torch.from_numpy(self.images))
 
         # Loading and preprocessing labels
-        self.labels = torch.LongTensor(data_dict[b"fine_labels"])
+        self.labels = torch.LongTensor(self.labels)
         self.labels = F.one_hot(self.labels, num_classes=100).type(torch.float32)
-
-        # Splitting data
-        index_split = int(train_val_split * self.images.shape[0])
-        if split == "train":
-            self.images = self.images[:index_split]
-            self.labels = self.labels[:index_split]
-        elif split == "dev":
-            self.images = self.images[index_split:]
-            self.labels = self.labels[index_split:]
 
     def __len__(self):
         return self.images.shape[0]
