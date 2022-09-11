cb3723b8f84fb1e316e9e72a909c56f65c806731
pedrocg42 <pedrocorglez@gmail.com>
2022-09-11 19:31:31 +0200

    feat: change dataset path

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-dataset_path = "G:/Pedro/Images/CIFAR100/cifar-100-python/cifar-100-python"
+dataset_path = "G:/Pedro/Images/CIFAR100/"
 
 models_path = "results/models"
 os.makedirs(models_path, exist_ok=True)
