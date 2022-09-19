37c4e29346f6dddb85093ef2712f02e4a1f1a959
pedrocg42 <pedrocorglez@gmail.com>
2022-09-20 01:13:48 +0200

    update train.bat

--- train.bat
+++ train.bat
@@ -1,3 +1,4 @@
+
 python train.py --experiment dkvb_resnet50_1_pretrain
 python train.py --experiment vq_resnet50_1_pretrain
 python train.py --experiment dkvb_resnet50_64_pretrain
@@ -7,4 +8,95 @@ python train.py --experiment vq_resnet50_128_pretrain
 python train.py --experiment dkvb_resnet50_512_pretrain
 python train.py --experiment vq_resnet50_512_pretrain
 python train.py --experiment baseline_resnet50_pretrain
-python train.py --experiment baseline_resnet50_pretrain_encoder
+python train.py --experiment baseline_resnet50_encoder_pretrain
+
+@REM Experiment 1
+python train_exp.py --experiment dkvb_resnet50_1_exp1_class_0
+python train_exp.py --experiment vq_resnet50_1_exp1_class_0
+python train_exp.py --experiment dkvb_resnet50_64_exp1_class_0
+python train_exp.py --experiment vq_resnet50_64_exp1_class_0
+python train_exp.py --experiment dkvb_resnet50_128_exp1_class_0
+python train_exp.py --experiment vq_resnet50_128_exp1_class_0
+python train_exp.py --experiment dkvb_resnet50_512_exp1_class_0
+python train_exp.py --experiment vq_resnet50_512_exp1_class_0
+python train_exp.py --experiment baseline_resnet50_exp1_class_0
+python train_exp.py --experiment baseline_resnet50_encoder_exp1_class_0
+
+python train_exp.py --experiment dkvb_resnet50_1_exp1_class_28
+python train_exp.py --experiment vq_resnet50_1_exp1_class_28
+python train_exp.py --experiment dkvb_resnet50_64_exp1_class_28
+python train_exp.py --experiment vq_resnet50_64_exp1_class_28
+python train_exp.py --experiment dkvb_resnet50_128_exp1_class_28
+python train_exp.py --experiment vq_resnet50_128_exp1_class_28
+python train_exp.py --experiment dkvb_resnet50_512_exp1_class_28
+python train_exp.py --experiment vq_resnet50_512_exp1_class_28
+python train_exp.py --experiment baseline_resnet50_exp1_class_28
+python train_exp.py --experiment baseline_resnet50_encoder_exp1_class_28
+
+python train_exp.py --experiment dkvb_resnet50_1_exp1_class_99
+python train_exp.py --experiment vq_resnet50_1_exp1_class_99
+python train_exp.py --experiment dkvb_resnet50_64_exp1_class_99
+python train_exp.py --experiment vq_resnet50_64_exp1_class_99
+python train_exp.py --experiment dkvb_resnet50_128_exp1_class_99
+python train_exp.py --experiment vq_resnet50_128_exp1_class_99
+python train_exp.py --experiment dkvb_resnet50_512_exp1_class_99
+python train_exp.py --experiment vq_resnet50_512_exp1_class_99
+python train_exp.py --experiment baseline_resnet50_exp1_class_99
+python train_exp.py --experiment baseline_resnet50_encoder_exp1_class_99
+
+@REM Experiment 2
+python train_exp.py --experiment dkvb_resnet50_1_exp2_samples_50
+python train_exp.py --experiment vq_resnet50_1_exp2_samples_50
+python train_exp.py --experiment dkvb_resnet50_64_exp2_samples_50
+python train_exp.py --experiment vq_resnet50_64_exp2_samples_50
+python train_exp.py --experiment dkvb_resnet50_128_exp2_samples_50
+python train_exp.py --experiment vq_resnet50_128_exp2_samples_50
+python train_exp.py --experiment dkvb_resnet50_512_exp2_samples_50
+python train_exp.py --experiment vq_resnet50_512_exp2_samples_50
+python train_exp.py --experiment baseline_resnet50_exp2_samples_50
+python train_exp.py --experiment baseline_resnet50_encoder_exp2_samples_50
+
+python train_exp.py --experiment dkvb_resnet50_1_exp2_samples_70
+python train_exp.py --experiment vq_resnet50_1_exp2_samples_70
+python train_exp.py --experiment dkvb_resnet50_64_exp2_samples_70
+python train_exp.py --experiment vq_resnet50_64_exp2_samples_70
+python train_exp.py --experiment dkvb_resnet50_128_exp2_samples_70
+python train_exp.py --experiment vq_resnet50_128_exp2_samples_70
+python train_exp.py --experiment dkvb_resnet50_512_exp2_samples_70
+python train_exp.py --experiment vq_resnet50_512_exp2_samples_70
+python train_exp.py --experiment baseline_resnet50_exp2_samples_70
+python train_exp.py --experiment baseline_resnet50_encoder_exp2_samples_70
+
+python train_exp.py --experiment dkvb_resnet50_1_exp2_samples_100
+python train_exp.py --experiment vq_resnet50_1_exp2_samples_100
+python train_exp.py --experiment dkvb_resnet50_64_exp2_samples_100
+python train_exp.py --experiment vq_resnet50_64_exp2_samples_100
+python train_exp.py --experiment dkvb_resnet50_128_exp2_samples_100
+python train_exp.py --experiment vq_resnet50_128_exp2_samples_100
+python train_exp.py --experiment dkvb_resnet50_512_exp2_samples_100
+python train_exp.py --experiment vq_resnet50_512_exp2_samples_100
+python train_exp.py --experiment baseline_resnet50_exp2_samples_100
+python train_exp.py --experiment baseline_resnet50_encoder_exp2_samples_100
+
+@REM Experiment 3
+python train_exp.py --experiment dkvb_resnet50_1_exp3_reps_20
+python train_exp.py --experiment vq_resnet50_1_exp3_reps_20
+python train_exp.py --experiment dkvb_resnet50_64_exp3_reps_20
+python train_exp.py --experiment vq_resnet50_64_exp3_reps_20
+python train_exp.py --experiment dkvb_resnet50_128_exp3_reps_20
+python train_exp.py --experiment vq_resnet50_128_exp3_reps_20
+python train_exp.py --experiment dkvb_resnet50_512_exp3_reps_20
+python train_exp.py --experiment vq_resnet50_512_exp3_reps_20
+python train_exp.py --experiment baseline_resnet50_exp3_reps_20
+python train_exp.py --experiment baseline_resnet50_encoder_exp3_reps_20
+
+python train_exp.py --experiment dkvb_resnet50_1_exp3_reps_10
+python train_exp.py --experiment vq_resnet50_1_exp3_reps_10
+python train_exp.py --experiment dkvb_resnet50_64_exp3_reps_10
+python train_exp.py --experiment vq_resnet50_64_exp3_reps_10
+python train_exp.py --experiment dkvb_resnet50_128_exp3_reps_10
+python train_exp.py --experiment vq_resnet50_128_exp3_reps_10
+python train_exp.py --experiment dkvb_resnet50_512_exp3_reps_10
+python train_exp.py --experiment vq_resnet50_512_exp3_reps_10
+python train_exp.py --experiment baseline_resnet50_exp3_reps_10
+python train_exp.py --experiment baseline_resnet50_encoder_exp3_reps_10
