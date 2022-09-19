f9410b83ed0e6a83ec27bdb88dc8cd2c5cf41f5b
pedrocg42 <pedrocorglez@gmail.com>
2022-09-20 01:11:34 +0200

    fixing errors in data.py

--- data.py
+++ data.py
@@ -70,7 +70,7 @@ class CIFAR100(Dataset):
 
 
 class CIFAR100Exp1(Dataset):
-    def __init__(self, train_val_split: float = 0.8, class_num: int = 42, steps: int = 250):
+    def __init__(self, class_num: int, train_val_split: float, steps: int, **kwargs):
 
         self.train_val_split = train_val_split
         self.class_num = class_num
@@ -117,14 +117,15 @@ class CIFAR100Exp1(Dataset):
         return self.length * self.steps
 
     def __getitem__(self, index):
-        return self.images[index % self.images.shape[0]], self.labels[index % self.images.shape[0]]
+        return self.images[index % self.length], self.labels[index % self.length]
 
 
 class CIFAR100Exp2(Dataset):
-    def __init__(self, train_val_split: float = 0.8, images_per_class: int = 50):
+    def __init__(self, train_val_split: float, images_per_class: int, steps: int, **kwargs):
 
         self.train_val_split = train_val_split
         self.images_per_class = images_per_class
+        self.steps = steps
 
         # Loading raw file
         file_path = os.path.join(config.dataset_path, "train")
@@ -163,18 +164,22 @@ class CIFAR100Exp2(Dataset):
         self.labels = torch.LongTensor(self.labels)
         self.labels = F.one_hot(self.labels, num_classes=100).type(torch.float32)
 
+        # Extracting dataset length
+        self.length = self.images.shape[0]
+
     def __len__(self):
-        return self.images.shape[0]
+        return 100 * self.steps  # batch_size
 
     def __getitem__(self, index):
-        return self.images[index], self.labels[index]
+        return self.images[index % self.length], self.labels[index % self.length]
 
 
 class CIFAR100Exp3(Dataset):
-    def __init__(self, train_val_split: float = 0.8, images_per_class: int = 50):
+    def __init__(self, train_val_split: float, repetitions_per_class: int, steps: int, **kwargs):
 
         self.train_val_split = train_val_split
-        self.images_per_class = images_per_class
+        self.repetitions_per_class = repetitions_per_class
+        self.steps = steps
 
         # Loading raw file
         file_path = os.path.join(config.dataset_path, "train")
@@ -196,11 +201,9 @@ class CIFAR100Exp3(Dataset):
         # Only taking val
         self.indexes = self.indexes[:, index_split:]
 
-        # Only taking some images per class
-        self.indexes = self.indexes[:, : self.images_per_class].flatten()
-
-        self.images = self.images[self.indexes]
-        self.labels = self.labels[self.indexes]
+        # Repeating r times the indexes of each class
+        self.indexes = np.tile(self.indexes, (1, self.repetitions_per_class))
+        self.indexes = self.indexes.flatten()
 
         # Preprocessing images
         self.images = self.images.reshape(self.images.shape[0], 3, 32, 32)
@@ -213,8 +216,12 @@ class CIFAR100Exp3(Dataset):
         self.labels = torch.LongTensor(self.labels)
         self.labels = F.one_hot(self.labels, num_classes=100).type(torch.float32)
 
+        # Extracting dataset length (batch_size)
+        self.length = self.images.shape[0]
+
     def __len__(self):
-        return self.images.shape[0]
+        return 100 * self.steps
 
     def __getitem__(self, index):
-        return self.images[index], self.labels[index]
+        i = self.indexes[index % len(self.indexes)]
+        return self.images[i], self.labels[i]
